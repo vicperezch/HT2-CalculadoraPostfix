9094ca2c7ab507bbc0fce5f34da35999b9385b3d
nilsmuralles <murallesnils11@gmail.com>
2024-01-20 22:11:50 -0600

    Añadiendo interfaz para la pila

---
+++ OurCalculator/IStack.java
@@ -0,0 +1,11 @@
+/**
+ * @author Nils Muralles & Víctor Pérez
+ * @version 1.0.0
+ * @since 20/01/24
+ * Interfaz que describe los métodos para el funcionamiento de la pila
+ */
+
+public interface IStack<T> {
+    void push(T value);
+    T pop();
+}
---
+++ OurCalculator/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Nils Muralles & Víctor Pérez
+ * @version 1.0.0
+ * @since 20/01/24
+ * Clase principal del programa
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
